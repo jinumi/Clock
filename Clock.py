fb86dc1a91f6e4ed2f4cec4885ee52dfe1a20e40
Muhammad Umair Yaqub <umairyaqubofficial@gmail.com>
2023-03-27 20:36:48 +0500

    Modernization Update

--- Clock.py
+++ Clock.py
@@ -8,6 +8,8 @@ from tkinter import *
 import tkinter as TK
 
 # Create a Drag class to allow the window to be dragged around the screen
+
+
 class Drag(TK.Tk):
     def __init__(self):
         super().__init__()
@@ -28,6 +30,7 @@ class Drag(TK.Tk):
         self._offsetx = super().winfo_pointerx() - super().winfo_rootx()
         self._offsety = super().winfo_pointery() - super().winfo_rooty()
 
+
 # Create an instance of the Drag class to create the main window
 window = Drag()
 
@@ -43,17 +46,23 @@ close_btn = Label(window, width=50, height=50, bg="#ededed")
 close_btn.grid(row=1, column=3)
 
 # Create the close button and place it on the label
-close = Button(close_btn, text="✕", anchor='e', justify=LEFT, font=('Roboto', 10, 'normal'), fg="#737373", width=2, height=1, bd=0, bg="#ededed", cursor="hand2", command=window.destroy).grid(row=1, column=3, padx=2, pady=1)
+close = Button(close_btn, text="✕", anchor='e', justify=LEFT, font=('Roboto', 10, 'normal'), fg="#737373", width=2,
+               height=1, bd=0, bg="#ededed", cursor="hand2", command=window.destroy).grid(row=1, column=3, padx=2, pady=1)
 
 # Create labels for the date, time, and meridiem and place them on the window
-Date = Label(window, font=('Roboto', 15, 'normal'), fg="#737373", bg="#ededed", bd=0, justify=LEFT)
+Date = Label(window, font=('Roboto', 15, 'normal'),
+             fg="#737373", bg="#ededed", bd=0, justify=LEFT)
 Date.grid(row=1, column=1)
-Time = Label(window, font=('Roboto', 30, 'normal'), fg="#737373", bg="#ededed", bd=0, justify=LEFT)
+Time = Label(window, font=('Roboto', 30, 'normal'),
+             fg="#737373", bg="#ededed", bd=0, justify=LEFT)
 Time.grid(row=2, column=1)
-Meridiem = Label(window, font=('Roboto', 13, 'normal'), fg="#737373", bg="#ededed", bd=0, justify=CENTER)
+Meridiem = Label(window, font=('Roboto', 13, 'normal'),
+                 fg="#737373", bg="#ededed", bd=0, justify=CENTER)
 Meridiem.grid(row=2, column=2)
 
 # Define a function to update the labels with the current date and time
+
+
 def date():
     date_input = time.strftime(" %a, %d %b")
     time_input = time.strftime("\n%#I:%M")
@@ -61,6 +70,7 @@ def date():
     Date.config(text=date_input)
     Time.config(text=time_input)
     Meridiem.config(text=meridiem_input.lower())
+
 
 # Call the date function to initialize the labels with the current date and time
 date()
